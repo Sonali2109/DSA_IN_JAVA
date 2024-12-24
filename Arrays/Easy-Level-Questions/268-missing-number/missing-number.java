a079cb674ad4ed92e4d4684926ccb62397b3325e
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2024-12-24 17:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (8.21%) - LeetSync

--- Arrays/Easy-Level-Questions/268-missing-number/missing-number.java
+++ Arrays/Easy-Level-Questions/268-missing-number/missing-number.java
@@ -1,6 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) 
     {
+        // OPTIMAL APPROACH 1 - XOR Method
+        int res = nums.length;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            res ^= i;
+            res ^= nums[i];
+        }
+        
+        return res;
+
+    /*    
+        // OPTIMAL APPROACH 2 - Sum Method
         int n = nums.length;
         int sum = (n * (n+1)) / 2;
         int s2 = 0;
@@ -11,5 +24,6 @@ class Solution {
         }
         
         return (sum-s2);
+    */
     }
 }
