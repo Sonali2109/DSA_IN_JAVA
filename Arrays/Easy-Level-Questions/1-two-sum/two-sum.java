1c812d47e1a749ee983eae85aa361a57daba4a60
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2025-01-02 21:03:58 +0530

    Time: 2 ms (98.96%) | Memory: 45.2 MB (22.48%) - LeetSync

--- Arrays/Easy-Level-Questions/1-two-sum/two-sum.java
+++ Arrays/Easy-Level-Questions/1-two-sum/two-sum.java
@@ -1,6 +1,30 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) 
+    { 
+        // Better Approach - Hashing
 
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+
+        for(int i=0; i<nums.length; i++) 
+        {
+            int a = nums[i];
+            int more = target - a;
+
+            if(hashMap.containsKey(more)){
+                ans[0] = hashMap.get(more);
+                ans[1] = i;
+                return ans;
+            }
+
+            hashMap.put(nums[i],i);
+        }
+
+        return ans;
+
+/*
+        // my approach - TWO POINTERS APPROACH
         int i=0;
         int j=nums.length-1;
 
@@ -22,5 +46,7 @@ class Solution {
         }
 
         return new int[]{-1};
+*/
+
     }
 }
