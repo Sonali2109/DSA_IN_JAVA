50e3a60699edf75388f4500f35a014090c7fa6b7
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2025-01-03 16:33:38 +0530

    Time: 1 ms (99.40%) | Memory: 57.1 MB (39.07%) - LeetSync

--- Arrays/Easy-Level-Questions/53-maximum-subarray/maximum-subarray.java
+++ Arrays/Easy-Level-Questions/53-maximum-subarray/maximum-subarray.java
@@ -18,6 +18,23 @@ class Solution {
         }
 
         return (int)maxi;
+/*
+        // Brute Approach
+        int maxi = Integer.MIN_VALUE; 
+        int n = nums.length;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+
+                maxi = Math.max(maxi, sum);
+            }
+        }
+
+        return maxi;
+*/
     }
 }
