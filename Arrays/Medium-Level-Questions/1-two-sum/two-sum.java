621ca498320b7ce0433f105f7510a097cbb1eda9
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2025-01-13 18:59:10 +0530

    Time: 3 ms (65.42%) | Memory: 44.9 MB (55.87%) - LeetSync

--- Arrays/Medium-Level-Questions/1-two-sum/two-sum.java
+++ Arrays/Medium-Level-Questions/1-two-sum/two-sum.java
@@ -1,5 +1,27 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) 
+    {
+        // BETTER APPROACH - Using Hashing (HashMap)
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        int ans[] = new int[2];
+        ans[0] = ans[1] = -1;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            int require = target - nums[i];
+            
+            if(hashMap.containsKey(require)){
+                ans[0] = hashMap.get(require);
+                ans[1] = i;
+                return ans;
+            }
+            hashMap.put(nums[i],i);
+        }
+
+        return ans;
+
+/*
+        // BRUTE APPROACH
         for(int i=0; i<nums.length; i++)
         {
             for(int j=0; j<nums.length; j++)
@@ -10,7 +32,7 @@ class Solution {
                 }
             }
         }
-
         return new int[]{-1,-1};
+*/
     }
 }
