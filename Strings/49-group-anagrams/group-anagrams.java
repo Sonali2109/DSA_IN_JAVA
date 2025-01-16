04d7341d8b5c2b22008db39d0ac8adfb093ad530
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2025-01-16 07:26:20 +0530

    Time: 6 ms (98.04%) | Memory: 48.3 MB (23.88%) - LeetSync

--- Strings/49-group-anagrams/group-anagrams.java
+++ Strings/49-group-anagrams/group-anagrams.java
@@ -1,7 +1,38 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0)
+            return new ArrayList<>();
+
+    /*
+        // Solution 1
+        HashMap<String, List> map = new HashMap<>();
+
+        for(String s : strs)
+        {
+            int[] count = new int[26];
+            for(char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(int i : count) {
+                sb.append("#");
+                sb.append(i);
+            }
+
+            String key = sb.toString();
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<String>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList(map.values());
+    */
+        // Solution 2
         HashMap<String, ArrayList<String>> map = new HashMap<>();
-        for(String str : strs) {
+        for(String str : strs) 
+        {
             char[] chars = str.toCharArray();
             Arrays.sort(chars);
             String sortedChars = new String(chars);
@@ -10,6 +41,7 @@ class Solution {
             }
             map.get(sortedChars).add(str);
         }
+
         return new ArrayList<>(map.values());
     }
 }
