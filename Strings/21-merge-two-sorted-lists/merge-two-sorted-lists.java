87c3e53e5cb694e1ff2ce6bf2c4cb23f9d5802b3
Sonali Chaudhari <108192762+Sonali2109@users.noreply.github.com>
2025-01-28 07:34:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.55%) - LeetSync

--- Strings/21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ Strings/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,42 +10,21 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        //Returning lists if one or both list is empty
-        if(list1 == null && list2 == null){
-            return list1;
-        }else if(list1 == null){
+        if(list1 == null){
             return list2;
-        }else if(list2 == null){
+        }
+
+        if(list2== null){
             return list1;
         }
-
-        //New Sorted list
-        ListNode queryingDummy = new ListNode();
-
-        //New Head for result
-        ListNode newHead = queryingDummy;
-        
-        while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                queryingDummy.next = list1;
-                list1 = list1.next;
-            }else{
-                queryingDummy.next = list2;
-                list2 = list2.next;
-            }
-            queryingDummy= queryingDummy.next;
-        }
-
-        //Setting remaing list in the resulted sorted list
-        if(list1==null && list2!=null){
-            queryingDummy.next = list2;
-        }else if(list1!=null && list2==null){
-            queryingDummy.next = list1;
-
-        }
-
-        return newHead.next;
-        
+        ListNode mergedList = null;
+        if(list1.val <= list2.val){
+            mergedList = new ListNode(list1.val);
+            mergedList.next = mergeTwoLists(list1.next, list2);
+        }else{
+            mergedList = new ListNode(list2.val);
+            mergedList.next = mergeTwoLists(list1, list2.next);
+        }      
+        return mergedList;
     }
 }
